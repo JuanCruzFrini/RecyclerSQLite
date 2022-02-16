daab66f8c9c1b06b64a712a97e15cb1a4ca09940
JuanCruzFrini <cucu_2009@live.com.ar>
2022-02-16 12:37:53 -0300

    commit

--- app/src/main/java/com/example/recyclersqlite/NuevoActivity.kt
+++ app/src/main/java/com/example/recyclersqlite/NuevoActivity.kt
@@ -2,55 +2,43 @@ package com.example.recyclersqlite
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.recyclersqlite.databinding.ActivityNuevoBinding
 
 class NuevoActivity : AppCompatActivity() {
 
-    lateinit var txtNombre: EditText
-    lateinit var txtTelefono:EditText
-    lateinit var txtCorreoElectronico:EditText
-    lateinit var btnGuarda: Button
+    lateinit var binding: ActivityNuevoBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_nuevo)
-        txtNombre = findViewById(R.id.txtNombre)
-        txtTelefono = findViewById(R.id.txtTelefono)
-        txtCorreoElectronico = findViewById(R.id.txtCorreoElectronico)
-        btnGuarda = findViewById(R.id.btnGuarda)
-        btnGuarda.setOnClickListener {
-            if (txtNombre.text.toString() != "" && txtTelefono.text.toString() != "") {
-                val dbContactos = DbContactos(this@NuevoActivity)
+        binding = ActivityNuevoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnGuarda.setOnClickListener {
+            if (binding.txtNombre.text.toString() != "" && binding.txtTelefono.text.toString() != "") {
+                val dbContactos = DbContactos(this)
                 val id = dbContactos.insertarContacto(
-                    txtNombre.text.toString(),
-                    txtTelefono.text.toString(),
-                    txtCorreoElectronico.text.toString()
+                    binding.txtNombre.text.toString(),
+                    binding.txtTelefono.text.toString(),
+                    binding.txtCorreoElectronico.text.toString()
                 )
                 if (id > 0) {
-                    Toast.makeText(this@NuevoActivity, "REGISTRO GUARDADO", Toast.LENGTH_LONG).show()
+                    Toast.makeText(this, "REGISTRO GUARDADO", Toast.LENGTH_LONG).show()
                     limpiar()
                 } else {
-                    Toast.makeText(this@NuevoActivity, "ERROR AL GUARDAR REGISTRO", Toast.LENGTH_LONG).show()
+                    Toast.makeText(this, "ERROR AL GUARDAR REGISTRO", Toast.LENGTH_LONG).show()
                 }
             } else {
-                Toast.makeText(this@NuevoActivity, "DEBE LLENAR LOS CAMPOS OBLIGATORIOS", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "DEBE LLENAR LOS CAMPOS OBLIGATORIOS", Toast.LENGTH_LONG).show()
             }
         }
     }
 
     private fun limpiar() {
-        txtNombre.setText("")
-        txtTelefono.setText("")
-        txtCorreoElectronico.setText("")
+        binding.txtNombre.setText("")
+        binding.txtTelefono.setText("")
+        binding.txtCorreoElectronico.setText("")
     }
-
-    override fun onBackPressed() {
-
-        startActivity(Intent(this, MainActivity::class.java))
-        super.onBackPressed()
-
-    }
+    override fun onBackPressed() { startActivity(Intent(this, MainActivity::class.java)) }
 }
